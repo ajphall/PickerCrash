638cfa16d339efe5aab3cc42e66469a4c0122a0a
Andrew Hall <andrew@hh-dev.com>
2022-06-28 02:18:12 -0700

    init

---
+++ PickerCrash/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PickerCrash;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PickerCrash/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PickerCrash;
+
+public class TestModel
+{
+    public string Name { get; set; }
+}
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<TestModel> Test { get; } = new ObservableCollection<TestModel>();
+
+    public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+        Test.Add(new TestModel());
+    }	
+}
+
